5de01bc3c7144a4b895942af2f7a7f69146582a9
Emma <emmaculate.jeyarajah@hotmail.com>
2021-07-20 12:40:16 +0100

    changing times to UTC

--- AzuruBot/src/events/ready/ReadyEvent.js
+++ AzuruBot/src/events/ready/ReadyEvent.js
@@ -10,21 +10,21 @@ module.exports = class ReadyEvent extends BaseEvent {
   async run (client) {
     console.log(client.user.tag + ' has logged in.');
 
-    let scheduledGoodMorningMessage = new cron.schedule('00 09 * * * *', () => {
+    let scheduledGoodMorningMessage = new cron.schedule('00 08 * * * *', () => {
       // Specifing your guild (server) and your channel
         const guild = client.guilds.cache.get('612030951219920898');
          const regularSpot = guild.channels.cache.get('612030951219920902');
          regularSpot.send('!goodMorning');
         });
     
-    let scheduledDadJokeMessage = new cron.schedule('00 13 * * * *', () => {
+    let scheduledDadJokeMessage = new cron.schedule('00 12 * * * *', () => {
       // Specifing your guild (server) and your channel
           const guild = client.guilds.cache.get('612030951219920898');
           const comedyCentral = guild.channels.cache.get('702122690999222373');
           comedyCentral.send('!dadJoke');
         });
       
-    let scheduledMotivationalMessage = new cron.schedule('35 12 * * * *', () => {
+    let scheduledMotivationalMessage = new cron.schedule('45 11 * * * *', () => {
       // Specifing your guild (server) and your channel
          const guild = client.guilds.cache.get('612030951219920898');
          const motivationalChannel = guild.channels.cache.get('701103240204255272');
