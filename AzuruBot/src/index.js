265dedcb65641bd93b6f8720d261dce3320dde38
Emma <emmaculate.jeyarajah@hotmail.com>
2021-07-27 16:27:14 +0100

    merge conflict

--- AzuruBot/src/index.js
+++ AzuruBot/src/index.js
@@ -7,9 +7,14 @@ const client = new Client({partials: ["MESSAGE", "CHANNEL", "REACTION"]});
 (async () => {
   client.commands = new Map();
   client.events = new Map();
-  client.prefix = process.env.PREFIX;
+  //client.prefix = process.env.PREFIX;
+  client.prefix = config.prefix;
   await registerCommands(client, '../commands');
   await registerEvents(client, '../events');
-  await client.login(process.env.CLIENT_TOKEN);
+  //await client.login(process.env.CLIENT_TOKEN);
+  await client.login(config.token)
+
+  app = express();
+  app.listen(3000);
 })();
 
